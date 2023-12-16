deb54f3467e5ce5ca5854710d97dc8185fbb97a6
ThePax007 <p.grzyb1995@gmail.com>
2023-12-16 20:49:51 +0100

    add code

---
+++ src/test/java/pl/tarnopolski/jexample/JexampleApplicationTest.java
@@ -0,0 +1,14 @@
+package pl.tarnopolski.jexample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JexampleApplicationTest {
+
+    @Test
+    public void added() {
+        JexampleApplication jexampleApplication = new JexampleApplication();
+        assertEquals(10, jexampleApplication.added(5,5));
+    }
+}
